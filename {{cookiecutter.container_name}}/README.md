c8a765883a917e2d7b02a08294df988ee5903116
Erik Tollerud <erik.tollerud@gmail.com>
2021-11-24 22:44:31 -0500

    minor adjustments to lab path/description

--- {{cookiecutter.container_name}}/README.md
+++ {{cookiecutter.container_name}}/README.md
@@ -9,3 +9,4 @@ To use this container:
 2. Run `./build_container.sh`
 n. `./run_container*.sh` and work inside the container's home directory, which maps to the host's `{{cookiecutter.content_dir}}` directory. Run the previous steps whenever the requirements change.
 
+If you are using jupyterlab and want to adjust its configuration you can put changes in the `{{cookiecutter.content_dir}}/.jupyter_config/user-settings` directory.
--- {{cookiecutter.container_name}}/run_container_jupyter.sh
+++ {{cookiecutter.container_name}}/run_container_jupyter.sh
@@ -6,4 +6,4 @@ docker run -it --rm --privileged \
            --mount type=bind,source="$(pwd)"/{{cookiecutter.content_dir}},target=/containerapp/{{cookiecutter.content_dir}} \
            -p 8888:8888 \
            {{cookiecutter.container_name}} \
-           jupyter lab  --ip="0.0.0.0" --notebook-dir="{{cookiecutter.content_dir}}" --LabApp.user_settings_dir="{{cookiecutter.content_dir}}/jupyter_config/user-settings" --LabApp.workspaces_dir="{{cookiecutter.content_dir}}/jupyter_config/workspaces"
+           jupyter lab  --ip="0.0.0.0" --notebook-dir="{{cookiecutter.content_dir}}" --LabApp.user_settings_dir="{{cookiecutter.content_dir}}/.jupyter_config/user-settings" --LabApp.workspaces_dir="{{cookiecutter.content_dir}}/.jupyter_config/workspaces"
