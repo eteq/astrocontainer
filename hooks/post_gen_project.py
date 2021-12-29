88adfff1132d3859b1a2a0149ebca67e2736659e
Erik Tollerud <erik.tollerud@gmail.com>
2021-12-29 15:32:29 -0500

    add containerexec_link option

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,14 @@
+import os
+import sys
+
+REMOVE_PATHS = [
+    '{% if cookiecutter.containerexec_link != "containerexec_link" %} containerexec_link {% endif %}',
+]
+
+for path in REMOVE_PATHS:
+    path = path.strip()
+    if path and os.path.exists(path):
+        if os.path.isdir(path):
+            os.rmdir(path)
+        else:
+            os.unlink(path)
