92847e08fb44d7a96a4ae9ae7f57a69ba5e2269d
Erik Tollerud <erik.tollerud@gmail.com>
2022-01-19 01:40:44 -0500

    podman now working correctly

--- {{cookiecutter.container_name}}/entrypoint_script.sh
+++ {{cookiecutter.container_name}}/entrypoint_script.sh
@@ -4,29 +4,45 @@
 
 set -e
 
+# skip the username wrangling if we are in podman and the user doesn't ask to override
+if [[ $container == "podman" && -z "$IGNORE_PODMAN_IN_ENTRYPOINT" ]]; then
+    echo "Detected podman, assuming rootless. Skipping user setup - if this is not what you wanted, set the IGNORE_PODMAN_IN_ENTRYPOINT environment variable when starting the container."
+    stay_root=true
+fi
+
+
 if [[ -z "$HOST_UID" ]]; then
-    echo "ERROR: please set HOST_UID" >&2
-    exit 1
+    echo "No HOST_UID, remaining root" >&2
+    stay_root=true
 fi
 if [[ -z "$HOST_GID" ]]; then
-    echo "ERROR: please set HOST_GID" >&2
-    exit 1
+    echo "No HOST_GID, remaining root" >&2
+    stay_root=true
 fi
 
-addgroup --gid "$HOST_GID" matchinguser
-adduser --uid "$HOST_UID" --gid "$HOST_GID" --gecos "" --disabled-password {{cookiecutter.container_name}}er
 
-cd /home/{{cookiecutter.container_name}}er
+if [ "$stay_root" = true ] ; then
+    cd $HOME
+else
+    addgroup --gid "$HOST_GID" matchinguser
+    adduser --uid "$HOST_UID" --gid "$HOST_GID" --gecos "" --disabled-password {{cookiecutter.container_name}}er
+    cd /home/{{cookiecutter.container_name}}er
+fi
+
 ln -s /containerapp/{{cookiecutter.content_dir}}
 
-# Drop privileges and execute next container command, or 'bash' if not specified. 
-if [[ $# -gt 0 ]]; then
-    if [ $1 = "bashroot" ]; then
-        # if first argument is bashroot, go to shell as root
-        bash
-    else
-        exec sudo -u {{cookiecutter.container_name}}er -- "$@"
-    fi
+if [ $1 = "bashroot" ]; then
+    # if first argument is bashroot, go to shell as root even if the user is set up
+    bash
+fi
+
+if [ "$stay_root" = true ] ; then
+    exec "$@"
 else
-    exec sudo -u {{cookiecutter.container_name}}er -- bash
+    # Drop privileges and execute next container command, or 'bash' if not specified. 
+    if [[ $# -gt 0 ]]; then
+        exec sudo -u {{cookiecutter.container_name}}er -- "$@"
+    else
+        exec sudo -u {{cookiecutter.container_name}}er -- bash
+    fi
 fi
--- {{cookiecutter.container_name}}/run_container_jupyter.sh
+++ {{cookiecutter.container_name}}/run_container_jupyter.sh
@@ -4,10 +4,14 @@ CONTAINER_EXEC=`dirname $0`/{{cookiecutter.container_executable}}
 {% else %}
 CONTAINER_EXEC={{cookiecutter.container_executable}}
 {% endif %}
+
+echo "You may need to use one of these ip addresses depending on how your container network is set up:"
+ifconfig | grep inet
+
 $CONTAINER_EXEC run -it --rm --privileged \
            -e HOST_UID="$(id -u)" \
            -e HOST_GID="$(id -g)" \
            --mount type=bind,source="$(pwd)"/{{cookiecutter.content_dir}},target=/containerapp/{{cookiecutter.content_dir}} \
            -p 8888:8888 \
            {{cookiecutter.container_name}} \
-           jupyter lab  --ip="0.0.0.0" --notebook-dir="{{cookiecutter.content_dir}}" --LabApp.user_settings_dir="{{cookiecutter.content_dir}}/.jupyter_config/user-settings" --LabApp.workspaces_dir="{{cookiecutter.content_dir}}/.jupyter_config/workspaces"
+           jupyter lab  --ip="0.0.0.0" --notebook-dir="{{cookiecutter.content_dir}}" --LabApp.user_settings_dir="{{cookiecutter.content_dir}}/.jupyter_config/user-settings" --LabApp.workspaces_dir="{{cookiecutter.content_dir}}/.jupyter_config/workspaces" --allow-root 
