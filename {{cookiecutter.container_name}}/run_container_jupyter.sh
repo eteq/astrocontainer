82bcefaf8f00b60a3fffda7e1d0ed18206b178d9
Erik Tollerud <erik.tollerud@gmail.com>
2022-01-20 13:13:59 -0500

    add the other-port option

--- {{cookiecutter.container_name}}/run_container_jupyter.sh
+++ {{cookiecutter.container_name}}/run_container_jupyter.sh
@@ -5,13 +5,17 @@ CONTAINER_EXEC=`dirname $0`/{{cookiecutter.container_executable}}
 CONTAINER_EXEC={{cookiecutter.container_executable}}
 {% endif %}
 
+EXTERNALPORT=${1:-8888}
+
 echo "You may need to use one of these ip addresses depending on how your container network is set up:"
 ifconfig | grep inet
+echo "Regardless of what the log below says, the port to connect to is: $EXTERNALPORT\n"
+echo "" # blank line to make the above a bit more prominent
 
 $CONTAINER_EXEC run -it --rm --privileged \
            -e HOST_UID="$(id -u)" \
            -e HOST_GID="$(id -g)" \
            --mount type=bind,source="$(pwd)"/{{cookiecutter.content_dir}},target=/containerapp/{{cookiecutter.content_dir}} \
-           -p 8888:8888 \
+           -p $EXTERNALPORT:8888 \
            {{cookiecutter.container_name}} \
            jupyter lab  --ip="0.0.0.0" --notebook-dir="{{cookiecutter.content_dir}}" --LabApp.user_settings_dir="{{cookiecutter.content_dir}}/.jupyter_config/user-settings" --LabApp.workspaces_dir="{{cookiecutter.content_dir}}/.jupyter_config/workspaces" --allow-root 
