80a9091e7c9cf4967ede995f23a5ca04cfe5f2ea
Erik Tollerud <erik.tollerud@gmail.com>
2022-02-10 17:21:40 -0500

    fix for bash way of running

--- {{cookiecutter.container_name}}/run_container_jupyter.sh
+++ {{cookiecutter.container_name}}/run_container_jupyter.sh
@@ -18,7 +18,7 @@ $CONTAINER_EXEC run -it --rm --privileged \
            --mount type=bind,source="$(pwd)"/{{cookiecutter.content_dir}},target=/containerapp/{{cookiecutter.content_dir}} \
            -p $EXTERNALPORT:8888 \
            {{cookiecutter.container_name}} \
-           jupyter lab  --ip="0.0.0.0" --notebook-dir="{{cookiecutter.content_dir}}" --LabApp.user_settings_dir="{{cookiecutter.content_dir}}/.jupyter_config/user-settings" --LabApp.workspaces_dir="{{cookiecutter.content_dir}}/.jupyter_config/workspaces" --allow-root 
+           "jupyter lab  --ip=\"0.0.0.0\" --notebook-dir=\"{{cookiecutter.content_dir}}\" --LabApp.user_settings_dir=\"{{cookiecutter.content_dir}}/.jupyter_config/user-settings\" --LabApp.workspaces_dir=\"{{cookiecutter.content_dir}}/.jupyter_config/workspaces\" --allow-root"
 
 if [ $? -eq 126 ]
 then
