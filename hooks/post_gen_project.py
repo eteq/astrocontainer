66620c4a0d0c30d1f0b0fa1e5c68de9071629df7
Erik Tollerud <erik.tollerud@gmail.com>
2022-05-10 11:47:17 -0400

    add function to auto-create git repo

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,5 +1,6 @@
 import os
 import sys
+from pathlib import Path
 
 REMOVE_PATHS = [
     '{% if cookiecutter.container_executable != "containerexec_link" %} containerexec_link {% endif %}',
@@ -16,4 +17,25 @@ for path in REMOVE_PATHS:
 {% if cookiecutter.container_executable == "podman" %}
 if os.path.exists('Dockerfile'):
     os.rename('Dockerfile', 'Containerfile')
+{% endif %}
+
+
+{% if cookiecutter.initialize_git == "yes" %}
+try:
+    from git import Repo
+except ImportError:
+    print("need gitpython to be installed to initialize git repo")
+    sys.exit(1)
+
+content_path = os.path.realpath('{{ cookiecutter.content_dir }}')
+repo = Repo.init(content_path)
+with open(os.path.join(content_path, '.gitignore'), 'w') as f:
+    f.write('data\n')
+    f.write('.ipynb_checkpoints\n')
+    f.write('__pycache__\n')
+    f.write('*.pyc\n')
+    f.write('*.pyo\n')
+
+repo.index.add('.gitignore')
+repo.index.commit('Initial add of .gitignore')
 {% endif %}
